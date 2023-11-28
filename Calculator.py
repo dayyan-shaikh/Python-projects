b8c76e72373cbb74ee2aa63ecdb19d25ea5aa5e3
Dayyan Shaikh <dayyanshaikh16@gmail.com>
2023-11-28 15:55:59 +0530

    commit

---
+++ Calculator.py
@@ -0,0 +1,12 @@
+a=int(input("Enter number 1: "))
+b=int(input("Enter number 2: "))
+
+print(f"Addition is: {a+b}")
+
+print(f"Subtraction is: {a-b}")
+
+print(f"Multiplication is: {a*b}")
+
+print(f"Division is: {a/b}")
+
+print(f"Modulus is: {a%b}")

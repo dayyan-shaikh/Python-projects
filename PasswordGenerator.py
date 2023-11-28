1db853d0195e85d4f44727623cd33ab34102be92
Dayyan Shaikh <dayyanshaikh16@gmail.com>
2023-11-28 15:40:59 +0530

    commit

---
+++ PasswordGenerator.py
@@ -0,0 +1,21 @@
+import random
+
+upper_letter="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+lower_letter=upper_letter.lower()
+digits="0123456789"
+
+upper,lower,nums=True,True,True
+
+all=""
+if upper:
+    all +=upper_letter
+if lower:
+    all +=lower_letter
+if nums:
+    all +=digits
+
+length=15
+
+
+password="".join(random.sample(all,length))
+print(password)
